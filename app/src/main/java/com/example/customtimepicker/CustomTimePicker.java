689b8170510e0ace242504829e411065b097340b
jeongdajeong <jisu5868@gmail.com>
2023-11-01 13:32:53 +0900

    Initial commit

---
+++ app/src/main/java/com/example/customtimepicker/CustomTimePicker.java
@@ -0,0 +1,78 @@
+package com.example.customtimepicker;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
+
+import java.lang.reflect.Field;
+
+public class CustomTimePicker extends LinearLayout {
+    private NumberPicker amPmPicker;
+    private NumberPicker hourPicker;
+    private NumberPicker minutePicker;
+    private int margin = 20; // 원하는 마진 크기 (픽셀)
+
+    public CustomTimePicker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomTimePicker(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        LayoutInflater.from(getContext()).inflate(R.layout.custom_time_picker, this, true);
+
+        amPmPicker = findViewById(R.id.amPmPicker);
+        hourPicker = findViewById(R.id.hourPicker);
+        minutePicker = findViewById(R.id.minutePicker);
+
+        // 설정 및 초기화
+        amPmPicker.setMinValue(0);
+        amPmPicker.setMaxValue(1);
+        amPmPicker.setDisplayedValues(new String[]{"오전", "오후"});
+
+        hourPicker.setMinValue(1);
+        hourPicker.setMaxValue(12);
+
+        minutePicker.setMinValue(0);
+        minutePicker.setMaxValue(59);
+
+        setPadding(50, 0, 50, 0);
+    }
+
+
+    public int getAmPm() {
+        return amPmPicker.getValue();
+    }
+
+    public int getHour() {
+        return hourPicker.getValue();
+    }
+
+    public int getMinute() {
+        return minutePicker.getValue();
+    }
+
+    public void setAmPm(int value) {
+        amPmPicker.setValue(value);
+    }
+
+    public void setHour(int value) {
+        hourPicker.setValue(value);
+    }
+
+    public void setMinute(int value) {
+        minutePicker.setValue(value);
+    }
+}
